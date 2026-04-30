7fa5f49d3897f600ebd8064a741390fca503feaa
Victor-DREC <victorpilachanga12@gmail.com>
2026-04-30 16:43:29 -0500

    Parte 2: caonfigurar tasa

--- simulador_completo.js
+++ simulador_completo.js
@@ -21,4 +21,14 @@ function mostrarSeccion(id) {
     let seccion = document.getElementById(id);
     seccion.classList.add("activa"); 
 }
-mostrarSeccion("parametros");
+
+function guardarTasa() {
+    let tasa = recuperarFloat("tasaInteres");
+
+    if (tasa >= 10 && tasa <= 20) {
+        tasaInteres = tasa;
+        mostrarTexto("mensajeTasa", "Tasa configurada correctamente: " + tasaInteres + "%");
+    } else {
+        mostrarTexto("mensajeTasa", "La tasa debe estar entre 10% y 20%");
+    }
+}
